a677d46bd80888b09c66d2f5eff7a6c2dc96ff1b
unknown <datp1907@gmail.com>
2023-04-20 16:55:24 +0700

    up

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpRequest = axios.create({
-  baseURL: process.env.REACT_API_URL_BACKEND,
+  baseURL: process.env.REACT_API_URL_BACKEND || "https://backend-comerce.onrender.com/api",
 });
 
 export default httpRequest;
