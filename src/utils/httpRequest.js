3fe7a1553c1ccbb2db07ffd17b482095aa6ff2f5
unknown <datp1907@gmail.com>
2023-04-20 14:09:53 +0700

    upload env

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.REACT_API_URL_BACKEND,
 });
 
 export default httpRequest;
